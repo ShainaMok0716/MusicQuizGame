3e2ee7d7fbabd479f6b9741f9d1317fc48356484
Shaina Mok <shainamok@gmail.com>
2022-08-24 14:50:33 -0400

    - remove unused reference

--- MusicQuiz/Assets/Scripts/Manager/AssetsManager.cs
+++ MusicQuiz/Assets/Scripts/Manager/AssetsManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.Collections;
-using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.Networking;
 
